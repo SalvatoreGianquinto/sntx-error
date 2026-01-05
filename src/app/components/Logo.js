fd8d84cf841db5b8da931fc94e2d911116161de7
SalvatoreGianquinto <gianquinto.salvatore11@gmail.com>
2026-01-05 19:16:20 +0100

    start pt.2

---
+++ src/app/components/Logo.js
@@ -0,0 +1,9 @@
+export default function Logo() {
+  return (
+    <div className="flex items-center text-2xl tracking-tighter select-none">
+      <span className="font-extrabold text-white uppercase">[SNTX</span>
+      <span className="font-thin ml-2 text-white/70 uppercase">ERROR]</span>
+      <div className="ml-3 w-2.5 h-7 bg-[#CCFF00] cursor-blink"></div>
+    </div>
+  )
+}
